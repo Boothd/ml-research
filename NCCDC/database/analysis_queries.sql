9db996990f79cca07f0a5df31b159c2a17cc5fec
Chris Sampson <cs_sampson@hotmail.com>
2016-05-27 16:38:30 +0100

    Add detail analysis query for communications between identified IPs; add analysis spreadsheet

--- NCCDC/database/analysis_queries.sql
+++ NCCDC/database/analysis_queries.sql
@@ -51,3 +51,23 @@ select
 from pcap_data
 group by dst, dst_ip /*, src, src_ip*/
 ;
+
+
+select * from (
+	(
+		select ts datetime, src_ip as attackerip, src as attackeripint, src_port as attackerport, dst_ip as victimip, dst as victimipint, dst_port as victimport, "Attacker -> Victim" as direction, flags, syn, ack, rst from pcap_data
+		where src = inet_aton('10.10.10.250') -- attacker ip
+		and dst = inet_aton('172.16.10.253') -- victim ip
+		and protocol = 6 -- TCP
+	)
+	union
+	(
+		select ts datetime, dst_ip as attackerip, dst as attackeripint, dst_port as attackerport, src_ip as victimip, src as victimipint, src_port as victimport, "Victim -> Attacker" as direction, flags, syn, ack, rst from pcap_data
+		where dst = inet_aton('10.10.10.250') -- attacker ip
+		and src = inet_aton('172.16.10.253') -- victim ip
+		and protocol = 6 -- TCP
+	)
+) dialogues 
+where victimport = 3301
+order by victimport, datetime
+;
