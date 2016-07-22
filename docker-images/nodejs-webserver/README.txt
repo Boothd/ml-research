2f301c0a19e6698c8f82a674b4cdc8e3b170bd93
Daniel Booth <was_100@hotmail.com>
2016-07-22 14:46:49 +0100

    implemented a simple service to test HTTP GET operations.

--- docker-images/nodejs-webserver/README.txt
+++ docker-images/nodejs-webserver/README.txt
@@ -14,3 +14,11 @@ Web Server
 	There is a webserver running which reports which server the app is pinging and how many requests is has made. The page is accessible on:
 
 		http://localhost:8888
+
+	A service for performing simple attacks on, ie http requests exists. This service has been setup to test the connectivity of attack scripts. Anything that perform a http GET on the URL below will increment a count held within the apps memory. The service is available on:
+
+		http://localhost:8888/attackme
+
+	To check the count, another url is available which simple reports the number of times /attackme has been accessed.
+
+		http://localhost:8888/attackcount
--- docker-images/nodejs-webserver/index.js
+++ docker-images/nodejs-webserver/index.js
@@ -1,26 +1,50 @@
 var request = require('request');
 var random = require("random-js")();
-var i = 0;
 var host = "http://localhost:8080"
+var express = require('express');
+var app = express();
+// var http = require('http');
+const PORT=8888; 
+var pingerCount = 0;
+var attackCount = 0;
+
+app.get('/attackme', function (req, res) {
+	attackCount++;
+  	res.send('thank you for attacking, please come again.');
+});
+
+app.get('/attackcount', function (req, res) {
+  res.send(
+  		'attackme has been accessed ' + attackCount + ' times <br/>'
+  	);
+});
+
+app.get('/', function (req, res) {
+  res.send(
+  		'Pinger has made ' + pingerCount + ' requests to the server '+ host +'<br/>'
+  	);
+});
+
+app.listen(PORT, function () {
+  console.log('server started on port '+ PORT);
+});
 
 /*
 Test
 */
-var http = require('http');
-const PORT=8888; 
 
-function handleRequest(request, response){
-    response.end('Pinger has made ' + i + ' requests to the server '+ host);
-}
-var server = http.createServer(handleRequest);
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+
+// function handleRequest(request, response){
+//     response.end('Pinger has made ' + i + ' requests to the server '+ host);
+// }
+// var server = http.createServer(handleRequest);
+// server.listen(PORT, function(){
+//     //Callback triggered when server is successfully listening. Hurray!
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
 
 function httpRequest(callback){
-	//console.log(i++);
-	i++;
+	pingerCount++;
 	request(host, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    //console.log(body) 
