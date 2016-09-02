355beb7dbe8e672719f076295ef4907fa45473a1
cs_sampson@hotmail.com <cs_sampson@hotmail.com>
2016-09-02 20:32:33 +0100

    Remove erroneous whitespace

--- docker-images/alpine-commander/scripts/start_run.sh
+++ docker-images/alpine-commander/scripts/start_run.sh
@@ -2,16 +2,16 @@
 
 # create new log directory using a UUID
 uuid=`uuidgen`
-base_dir=/usr/local/sbin/log 
-current_dir=${base_dir}/current        
-log_dir="${base_dir}/run-${uuid}"      
-mkdir -p ${log_dir}                         
-                                            
+base_dir=/log
+current_dir=${base_dir}/current
+log_dir=${base_dir}/run-${uuid}
+mkdir -p ${log_dir}
+
 # symlink new UUID log dir for "current" use
-if [ -e "${current_dir}" ]                  
-then                                  
-        rm ${current_dir}                                       
-fi                                                              
-ln -s ${log_dir} ${current_dir}                                 
-                                                                
+if [ -e "${current_dir}" ]
+then
+        rm ${current_dir}
+fi
+ln -s ${log_dir} ${current_dir}
+
 echo "Current log dir now ${log_dir}"
