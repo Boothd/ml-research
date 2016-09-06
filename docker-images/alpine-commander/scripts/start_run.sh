f56afa77424002e77ee2648f0cbf7e3805498131
cs_sampson@hotmail.com <cs_sampson@hotmail.com>
2016-09-06 17:22:18 +0100

    Correct commander CMD and ENTRYPOINT for script use

--- docker-images/alpine-commander/scripts/start_run.sh
+++ docker-images/alpine-commander/scripts/start_run.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-# create new log directory using a UUID
+echoerr() { echo "$@" >&2; }
+
+# generate UUID for use as new log directory
 uuid=`uuidgen`
 base_dir=/log
 current_dir=${base_dir}/current
-log_dir=${base_dir}/run-${uuid}
+log_dirname=run-${uuid}
+log_dir=${base_dir}/${log_dirname}
+
+# create new log directory (if does not already exist)
+if [[ -d "${log_dir}" ]]
+then
+	echoerr "Log dir ${uuid} already exists, cannot continue"
+	exit 1
+fi
 mkdir -p ${log_dir}
 
 # symlink new UUID log dir for "current" use
-if [ -e "${current_dir}" ]
+if [[ -e "${current_dir}" ]]
 then
+	echoerr "Removing existing current symlink"
         rm ${current_dir}
 fi
-ln -s ${log_dir} ${current_dir}
+
+# create symlink relative to the base directory
+cd ${base_dir}
+ln -s ${log_dirname} current
 
 echo "Current log dir now ${log_dir}"
